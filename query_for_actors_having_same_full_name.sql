7c26adee7ce11cc60d2222153d1ab4ca3b056c13
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-09-26 13:23:30 +0200

    count of unique actors

--- query_for_actors_having_same_full_name.sql
+++ query_for_actors_having_same_full_name.sql
@@ -2,7 +2,7 @@
 -- actor count is 200 and unique actor count is 199. ----
 --- this question is to help you find that one actor that is repeating ---
 
-SELECT first_name, last_name
+
+-- count of all actors
+SELECT count(*)
 FROM actor
-GROUP BY 1, 2
-HAVING count(*) > 1
