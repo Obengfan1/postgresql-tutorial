81623d40731e1c0a900c6246154d86c32de76ef2
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-09-26 13:28:03 +0200

    find count of actors and find count of unique actors having same name

--- query_for_actors_having_same_full_name.sql
+++ query_for_actors_having_same_full_name.sql
@@ -3,6 +3,22 @@
 --- this question is to help you find that one actor that is repeating ---
 
 
--- count of all actors
+-- count of all actors --
 SELECT count(*)
 FROM actor
+
+-- count of unique actors --
+SELECT
+    count(DISTINCT(first_name, last_name))
+FROM actor
+
+
+-- find count of actors and find count of unique actors having same name --
+
+SELECT
+      first_name,
+      last_name,
+      count(*) AS num_of_duplicate
+FROM actor
+GROUP BY 1, 2
+HAVING count(*) > 1
